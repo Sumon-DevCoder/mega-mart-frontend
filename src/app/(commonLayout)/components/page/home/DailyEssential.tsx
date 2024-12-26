cb3c73a02625d791583b31c38345a574d03fab0b
SumonRahman24 <mustafizurrahmansumon24@gmail.com>
2024-12-26 21:31:39 +0600

    DailyEssential section completed with responsive

--- src/app/(commonLayout)/components/page/home/DailyEssential.tsx
+++ src/app/(commonLayout)/components/page/home/DailyEssential.tsx
@@ -1,9 +1,57 @@
+"use client";
+
+import useGetProducts from "@/hooks/useGetProducts";
 import SectionTitle from "../../SectionTitle";
+import DynamicLoader from "@/app/components/DynamicLoader";
+import { TProduct } from "@/types/product.types";
+import Image from "next/image";
 
 const DailyEssential = () => {
+  const { data, isLoading } = useGetProducts("Premium Fruits");
+
+  const premiumFruits = data?.data?.result?.[0]?.products || [];
+
+  const topPremiumFruits = premiumFruits?.filter(
+    (fruit: TProduct) => fruit.brandType === "top"
+  );
+
+  console.log("premiumFruits", topPremiumFruits);
+
   return (
     <div>
       <SectionTitle subtitle="Daily" title="Essentials" />
+      {isLoading ? (
+        <DynamicLoader />
+      ) : (
+        <div className="grid grid-cols-2  md:grid-cols-3 lg:grid-cols-6 gap-4 mt-4 px-5 lg:px-0">
+          {topPremiumFruits?.map((fruit: TProduct) => (
+            <div
+              key={fruit._id}
+              className="overflow-hidden hover:border rounded-2xl cursor-pointer"
+            >
+              {/* Product Image */}
+              <div className="relative h-[187px] bg-[#F5F5F5] rounded-2xl">
+                <Image
+                  src={fruit?.img?.[0]}
+                  alt={fruit.name}
+                  fill
+                  className="object-cover p-4"
+                />
+              </div>
+
+              {/* Product Info */}
+              <div className="p-4 text-center">
+                <h3 className="text-md  font-semibold text-[#666666]">
+                  {fruit.name}
+                </h3>
+                <p className="text-[#222222] text-lg font-bold">
+                  Up to {fruit.discount}% OFF
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
--- src/app/(commonLayout)/components/page/home/TopElectronicsBrand.tsx
+++ src/app/(commonLayout)/components/page/home/TopElectronicsBrand.tsx
@@ -1,75 +1,3 @@
-// "use client";
-// import useGetProducts from "@/hooks/useGetProducts";
-// import React from "react";
-// import SectionTitle from "../../SectionTitle";
-// import { TProduct } from "@/types/product.types";
-// import Image from "next/image";
-// import DynamicLoader from "@/app/components/DynamicLoader";
-
-// const TopElectronicsBrand = () => {
-//   const { data, isLoading } = useGetProducts("Electronics");
-//   const electronics = data?.data?.result?.[0]?.products;
-
-//   const topElectronics = electronics?.filter(
-//     (electronic: TProduct) => electronic.brandType === "top"
-//   );
-
-//   console.log(topElectronics);
-
-//   console.log(electronics);
-//   return (
-//     <div>
-//       <SectionTitle subtitle="Top Electronics" title="Brands" />
-//       {isLoading ? (
-//         <DynamicLoader />
-//       ) : (
-//         <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-3 bg-white place-items-center gap-10 mt-10 mx-0  md:mx-6 lg:mx-0 m-auto">
-//           {topElectronics?.slice(0, 3).map((product: TProduct) => {
-//             return (
-//               <div
-//                 key={product?._id}
-//                 className="flex items-center p-4 rounded-lg shadow-md max-w-md mx-auto bg-[#313131]  w-[389px] h-[207px]"
-//               >
-//                 {/* <!-- Left Content --> */}
-//                 <div className="flex flex-col items-start space-y-2 ">
-//                   <span className="font-normal uppercase text-sm text-white bg-[#494949] py-2 px-5 rounded-lg">
-//                     {product?.brand}
-//                   </span>
-//                   <div className="flex items-center py-2 space-x-2">
-//                     <Image
-//                       src={product?.brandLogo}
-//                       alt={product.name}
-//                       height={50}
-//                       width={50}
-//                       className="w-12 h-12 object-cover rounded-xl  bg-white"
-//                     />
-//                   </div>
-//                   <p className="font-semibold text-lg text-white pl-0.5">
-//                     Up to {product?.discount}% OFF
-//                   </p>
-//                 </div>
-
-//                 {/* <!-- Right Product Image --> */}
-//                 <div className="ml-auto">
-//                   <Image
-//                     src={product?.img?.[0]}
-//                     alt={product.name}
-//                     height={500}
-//                     width={500}
-//                     className="w-24 h-44 object-cover rounded-lg"
-//                   />
-//                 </div>
-//               </div>
-//             );
-//           })}
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default TopElectronicsBrand;
-
 "use client";
 import useGetProducts from "@/hooks/useGetProducts";
 import React from "react";
@@ -80,8 +8,7 @@ import DynamicLoader from "@/app/components/DynamicLoader";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
-import "swiper/css/navigation";
-import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import { Autoplay, Pagination } from "swiper/modules";
 
 const TopElectronicsBrand = () => {
   const { data, isLoading } = useGetProducts("Electronics");
@@ -91,17 +18,14 @@ const TopElectronicsBrand = () => {
     (electronic: TProduct) => electronic.brandType === "top"
   );
 
-  console.log(topElectronics);
-
   return (
-    <div>
+    <div className="">
       <SectionTitle subtitle="Top Electronics" title="Brands" />
       {isLoading ? (
         <DynamicLoader />
       ) : (
         <Swiper
           spaceBetween={30}
-          centeredSlides={true}
           autoplay={{
             delay: 3000,
             disableOnInteraction: false,
@@ -109,15 +33,19 @@ const TopElectronicsBrand = () => {
           pagination={{
             clickable: true,
           }}
-          navigation={true}
-          modules={[Autoplay, Pagination, Navigation]}
+          breakpoints={{
+            0: { slidesPerView: 1 }, // 1 slide on small devices
+            768: { slidesPerView: 2 }, // 2 slides on medium devices
+            1024: { slidesPerView: 3 }, // 3 slides on large devices
+          }}
+          modules={[Autoplay, Pagination]}
           className="mySwiper mt-10"
         >
           {topElectronics?.map((product: TProduct) => (
             <SwiperSlide key={product?._id}>
-              <div className="flex items-center p-4 rounded-lg shadow-md max-w-md mx-auto bg-[#313131] w-[389px] h-[207px]">
+              <div className="flex justify-between gap-4 items-center p-4 rounded-lg shadow-md w-[390px] mx-auto bg-[#313131]  h-[207px]">
                 {/* Left Content */}
-                <div className="flex flex-col items-start space-y-2">
+                <div className="col-span-2 flex flex-col items-start space-y-2">
                   <span className="font-normal uppercase text-sm text-white bg-[#494949] py-2 px-5 rounded-lg">
                     {product?.brand}
                   </span>
@@ -136,7 +64,7 @@ const TopElectronicsBrand = () => {
                 </div>
 
                 {/* Right Product Image */}
-                <div className="ml-auto">
+                <div className="col-span-1">
                   <Image
                     src={product?.img?.[0]}
                     alt={product.name}
